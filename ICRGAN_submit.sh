4f8f96f2b0c866c7787f83b2fd6a044a09ca0ee6
Hugo Bloem <hugo.bloem@ed.ac.uk>
2021-11-24 10:58:25 +0000

    add GPM configs

---
+++ ICRGAN_submit.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# GE options
+#$ -N ICRGAN
+#$ -l h_rt=48:00:00
+#$ -pe gpu-titanx 1
+#$ -l h_vmem=32G
+#$ -wd /exports/csce/eddie/geos/groups/eddie_geos_eps_eip/Hugo/GAN/PyTorch-StudioGAN
+#$ -e /home/s1959730/qoutput/
+#$ -o /home/s1959730/qoutput/
+#$ -m beas
+#$ -M hugo.bloem@ed.ac.uk
+
+# initialise environment modules
+. /etc/profile.d/modules.sh
+
+# Load Python
+module load cuda/10.2.89
+module load anaconda
+source activate studioGAN
+
+WANDB_DIR="/exports/eddie/scratch/s1959730/wandb"
+
+python src/main.py \
+--train \
+-cfg src/configs/GPM/ICRGAN.yaml \
+-data ./data/gpm_37_32px/ \
+-save ./output/ \
+--save_fake_images \
+--num_workers 0 \
+--save_every 100
